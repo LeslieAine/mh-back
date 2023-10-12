288aed6fc319cb126f8b1dc81055b67e447b278d
LeslieAine <aineleslie@gmail.com>
2023-10-12 11:12:10 -0700

    Remove posts association from creator

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -4,6 +4,6 @@ class Creator < User
     # Associations
     has_many :contents # To track created content
     has_many :favorite_counts, class_name: 'User', foreign_key: 'favorite_creator_id' # To count favorited by users
-    has_many :posts # To track posts created by the creator
+    # has_many :posts # To track posts created by the creator
     has_many :orders # To handle orders
 end
