4df023f2468f5aa990eba00805584d7f46956fc1
LeslieAine <aineleslie@gmail.com>
2023-10-12 11:09:51 -0700

    Remove timestamp validation on post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   # Validations
-  validates :timestamp, :content, presence: true
+  validates :content, presence: true
 
 end
