d8e77b40d2469a69ccfefa16b2e5c5fd7888f777
LeslieAine <aineleslie@gmail.com>
2023-10-12 11:08:00 -0700

    Update users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,8 +14,8 @@ class Api::V1::UsersController < ApplicationController
 
    # GET /api/v1/users/:id
    def show
-    @user = User.find(params[:id])
-    render json: @user, status: :ok
+    # @user = User.find(params[:id])
+    # render json: @user, status: :ok
   end
 
   # GET /users/new
