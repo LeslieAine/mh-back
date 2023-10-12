70a41bc67dbc6abc2c8b402b891628d5f23d91e9
LeslieAine <aineleslie@gmail.com>
2023-10-12 11:09:00 -0700

    Update sessions and reg controllers to authorize users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,58 +1,34 @@
-# frozen_string_literal: true
-
-# class Users::Registrations::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-# end
-
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
-  # POST /users/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    sign_in(resource_name, resource)
+  private
+
+  def respond_with(resource, _options = {})
+    puts "Responding with resource: #{resource.inspect}"
+
+    return unless resource.persisted?
+
+    current_user = User.find(resource.id)
+    token = request.env['warden-jwt_auth.token']
 
     render json: {
-      status: { code: 200, message: 'Signed in successfully!', data: current_user }
+      status: { code: 200, message: 'Signed in successfully!', data: current_user, token: token }
     }, status: :ok
   end
 
-  # DELETE /users/sign_out
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    if signed_out
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split[1],
+                             Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+
+    if current_user
       render json: { status: 200, message: 'Signed out successfully!' }, status: :ok
     else
       render json: { status: 401, message: 'User has no active session!' }, status: :unauthorized
     end
   end
 
-  private
-
-  def auth_options
-    { scope: resource_name, recall: "#{controller_path}#new" }
+  def sign_in_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
-
