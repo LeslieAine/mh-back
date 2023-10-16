b33bd8dab4b1551254cd7357c44e51025c2af2ab
LeslieAine <aineleslie@gmail.com>
2023-10-16 08:06:34 -0700

    Update contents controller

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -6,46 +6,59 @@ class Api::V1::ContentsController < ApplicationController
   
     # Display a list of all available content
     def index
-      @contents = Content.all
+      @contents = Content.includes(:creator).order(created_at: :desc)
+      render json: @contents.to_json(include: { creator: { only: [:id, :username, :avatar] } })
     end
   
     # Show details of a specific content item
     def show
-        @content = Content.find(params[:id])
-        @client = current_user if current_user.present? && current_user.client?
-        @can_purchase = @client && @client.balance >= @content.price
+        # @content = Content.find(params[:id])
+        @content = Content.includes(:creator).find_by(id: params[:id])
+        if @content
+            render json: @content.to_json(include: { creator: { only: [:id, :username, :avatar] } })
+          else
+            render json: { error: 'Content not found' }, status: :not_found
+          end
+        # @client = current_client
+        # @can_purchase = @client && @client.balance >= @content.price
     
         # You can add additional logic here, such as checking if the client has already purchased this content.
     
-        if @content.present?
-          render json: {
-            content: @content,
-            client: @client,
-            can_purchase: @can_purchase
-          }
-        else
-          render json: { error: 'Content not found' }, status: :not_found
-        end
-      end
+    #     if @content.present?
+    #       render json: {
+    #         content: @content,
+    #         client: @client,
+    #         can_purchase: @can_purchase
+    #       }
+    #     else
+    #       render json: { error: 'Content not found' }, status: :not_found
+    #     end
+    #   end
     end
   
     # Creator's action: Create new content
     def create
-      content = current_user.contents.build(content_params)
-      if content.save
-        flash[:notice] = 'Content created successfully!'
-        redirect_to content
-      else
-        flash[:alert] = 'Failed to create content'
-        render :new
-      end
+        @creator = current_creator
+
+        if @creator.nil?
+            render json: { error: 'User not found' }, status: :unprocessable_entity
+        else
+
+
+        @post = @creator.contents.build(content_params)
+        if @post.save
+            render json: @post, status: :created
+        else
+            render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+        end
+        end
     end
   
     private
   
     # Strong parameters for content creation
     def content_params
-      params.require(:content).permit(:title, :description, :price, :url)
+      params.require(:content).permit(:title, :description, :price, :url, :creator_id, :isPaid)
     end
   
     # Find content by ID for show action
