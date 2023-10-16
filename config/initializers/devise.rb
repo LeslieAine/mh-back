9be08f7445cd1873d7a0c7b6ec02259dba41e6e2
LeslieAine <aineleslie@gmail.com>
2023-10-16 03:47:17 -0700

    Update schema file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -327,6 +327,13 @@ Devise.setup do |config|
     jwt.revocation_requests += [
       ['DELETE', %r{^/creators/sign_out$}]
     ]
+    # Dispatch and revocation requests for the Client model
+    jwt.dispatch_requests += [
+      ['POST', %r{^/clients/sign_in$}]
+    ]
+    jwt.revocation_requests += [
+      ['DELETE', %r{^/clients/sign_out$}]
+    ]
     jwt.expiration_time = 120.minutes.to_i
 end
 end
