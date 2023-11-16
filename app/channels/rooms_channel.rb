bd1170260e9fae8bcf1bf85c7641294a01ebd576
LeslieAine <aineleslie@gmail.com>
2023-11-16 05:09:31 -0800

    Add channel for chatrooms

---
+++ app/channels/rooms_channel.rb
@@ -0,0 +1,12 @@
+class RoomsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "rooms_channel"
+  end
+  def received(data)
+    RoomsChannel.broadcast_to('rooms_channel')
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
