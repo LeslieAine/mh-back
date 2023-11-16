5bce292754333194e9da973613f3a367d3797f04
LeslieAine <aineleslie@gmail.com>
2023-11-16 05:12:54 -0800

    Not used(yet) or wont be used. Code will be cleaned out in final branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,64 +4,20 @@ Rails.application.routes.draw do
         sessions: 'users/sessions',
         registrations: 'users/registrations'
       }
-  # devise_for :clients, controllers: {
-  #   sessions: 'clients/sessions', registrations: 'clients/registrations'
-  # }
-  # devise_for :creators, controllers: {
-  #   sessions: 'creators/sessions', registrations: 'creators/registrations'
-  # }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # devise_for :users, 
-  # path: '', path_names: {
-  #   sign_in: 'login',
-  #   sign_out: 'logout',
-  #   registration: 'signup'
-  # } 
-  #  controllers: {
-  #   sessions: 'users/sessions', registrations: 'users/registrations'
-  # }
   namespace :api do
     namespace :v1 do
-      # resources :users, only: [:index, :show, :create, :update, :destroy] do
-      #   member do
-      #     post 'update_avatar'
-      #   end
-      # end
-      # resources :users, only: [:update] do
-      #   post "/message_history", to: "messages#message_history"
-      #   post "/create_message", to: "messages#create_message"
-      #   get "/message_histories", to: "messages#message_histories"
-        # get "/matches/:recipient_id", to: "matches#get_match"
-      # end
-      # resources :rooms
-      # resources :users
-      resources :conversations, only: [:create, :index, :show]
-      resources :messages, only: [:index, :create]
-      resources :users, only: [:index, :show]
-      # post '/login', to: 'users#login'
-      # resources :chatrooms, only: [:index, :create, :show]
 
-  #   resources :messages, only: [:index, :create]
-  #   resources :users, only: [:index, :create] do
-  #     member do
-  #       get 'chatrooms'
-  #     end
-  #   end
-  #   resources :chatrooms, only: [:index, :create, :show]
-  # end
+      resources :notifications
+      resources :chats
 
-      # resources :chats, only: [:index, :create]
-      # resources :messages, only: [:create]
-      # resources :chatrooms do
-      #   resources :messages
-      # end 
+      get "/:user_id/mark-all-as-seen", to: "notifications#mark_all_as_seen"
 
+      resources :users, only: [:index, :show] do
+        resources :rooms
+        resources :notifications
+      end
 
-      # post "/users/:id", to: "users#index"
-      # resources :publishers
       resources :roles
 
       # Transactions controller routes
