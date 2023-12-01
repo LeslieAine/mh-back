5d653771046d9552cc13b15f63a50c8ac1def4c6
LeslieAine <aineleslie@gmail.com>
2023-12-01 04:06:34 -0800

    Add methods for orders actions

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::OrdersController < ApplicationController
   # before_action :authenticate_user!
-  before_action :set_order, only: [:accept, :reject]
+  before_action :set_order, only: [:accept, :reject, :fulfill]
 
   def create
     @user = current_user
@@ -42,23 +42,28 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def accept
+    @order.update!(accepted_by: current_user)
+
+    render json: @order, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
     # Implement logic to handle order acceptance
     # Move balance to the accepting user's account
-    ActiveRecord::Base.transaction do
-      begin
-        # Deduct the order price from the accepting user's balance
-        current_user.balance += @order.price
-        current_user.save!
+    # ActiveRecord::Base.transaction do
+    #   begin
+    #     # Deduct the order price from the accepting user's balance
+    #     current_user.balance += @order.price
+    #     current_user.save!
   
-        # Update the order to indicate acceptance
-        @order.update!(accepted_by: current_user)
+    #     # Update the order to indicate acceptance
+    #     @order.update!(accepted_by: current_user)
   
-        render json: @order, status: :ok
-      rescue StandardError => e
-        render json: { error: e.message }, status: :unprocessable_entity
-        raise ActiveRecord::Rollback
-      end
-    end
+    #     render json: @order, status: :ok
+    #   rescue StandardError => e
+    #     render json: { error: e.message }, status: :unprocessable_entity
+    #     raise ActiveRecord::Rollback
+    #   end
+    # end
   end
 
   def reject
@@ -95,6 +100,18 @@ class Api::V1::OrdersController < ApplicationController
     # render json: @made_orders
   end
 
+  def creator_fulfilled_orders
+    @user = current_user
+    orders = Order.where(accepted_by_id: @user.id).where.not(fulfilled: nil)
+    render json: orders, status: :ok
+  end
+
+  def client_fulfilled_orders
+    @user = current_user
+    orders = Order.where(ordered_by_id: @user.id).where.not(fulfilled: nil)
+    render json: orders, status: :ok
+  end
+
   def received_orders
     @user = current_user.id
     orders = Order.where(user_id: @user)
@@ -103,6 +120,29 @@ class Api::V1::OrdersController < ApplicationController
     # @received_orders = @user.received_orders
     # render json: @received_orders
   end
+
+  def fulfill
+    # Check if the order is accepted and owned by the current user
+    if @order.accepted_by == current_user
+      content = params[:content]
+
+      # Update the order's fulfillment content
+      @order.update!(fulfilled: content)
+
+      # Perform the transaction to move balance
+      ActiveRecord::Base.transaction do
+        current_user.balance += @order.price
+        current_user.save!
+      end
+
+      render json: @order, status: :ok
+    else
+      render json: { error: 'Unauthorized or order not accepted' }, status: :unauthorized
+    end
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
 
   # def pending_orders
   #   @user = User.find(params[:user_id])
