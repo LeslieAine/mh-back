c2a0eb1ebff1a0d9c0655584c0e884ae3556b3d2
LeslieAine <aineleslie@gmail.com>
2023-12-08 05:14:15 -0800

    Update routes and schema files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_131805) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_05_114055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_131805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "isPaid"
+    t.jsonb "purchases", default: []
     t.index ["user_id"], name: "index_contents_on_user_id"
   end
 
